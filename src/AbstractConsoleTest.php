90bbb6f2519d8c771386d177f98ebf9408e81e30
Andreas Sundqvist <andreas@stefna.is>
2020-06-15 15:51:44 +0200

    Add method to locate console binary

--- src/AbstractConsoleTest.php
+++ src/AbstractConsoleTest.php
@@ -16,20 +16,32 @@ abstract class AbstractConsoleTest extends TestCase
 			'APP_ENV' => 'testing',
 		];
 		if (self::$initScript && file_exists(self::$initScript)) {
-			Process::fromShellCommandline('php ' . self::$initScript, null, $env);
+			Process::fromShellCommandline('php ' . self::$initScript, null, $env)->run();
 		}
-
 		$process->run(null, $env);
 
 		if (self::$shutdownScript && file_exists(self::$shutdownScript)) {
-			Process::fromShellCommandline('php ' . self::$shutdownScript, null, $env);
+			Process::fromShellCommandline('php ' . self::$shutdownScript, null, $env)->run();
 		}
 		return $process;
 	}
 
-	public function runCommand(string $command, array $args): Process
+	public function runConsoleCommand(array $args): Process
 	{
-		array_unshift($args, $command);
+		array_unshift($args, $this->findConsole());
 		return $this->runProcess(new Process($args));
+	}
+
+	protected function findConsole(): string
+	{
+		$maxDepth = 6;
+		while($maxDepth) {
+			$base = dirname(__DIR__, $maxDepth);
+			if (file_exists($base . '/vendor/bin/console')) {
+				return $base . '/vendor/bin/console';
+			}
+			$maxDepth--;
+		}
+		throw new \RuntimeException('console command not found');
 	}
 }
