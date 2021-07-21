fb4abe151551b1679ea6e4c100fe7e480202cfcb
Andreas Sundqvist <andreas@stefna.is>
2021-07-21 13:07:25 +0200

    Increase wait timeout to 8 seconds

--- src/Endpoint/Server.php
+++ src/Endpoint/Server.php
@@ -85,7 +85,7 @@ final class Server
 		// Execute the command and store the process ID
 		exec($command, $output, $ret);
 		// Need a little time to make sure the php server is up and running
-		usleep(40000);
+		usleep(80000);
 
 		$pid = \is_file($pidFile) ? (int)\file_get_contents($pidFile) : 0;
 		if (!$pid || !\is_dir("/proc/$pid")) {
